b5c86baf4fb4598042853f3bca0cdba255015abe
Bryce Spraggins <spragbry93@gmail.com>
2021-06-07 15:48:22 -0400

    header code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-import logo from "../assets/images/ucf-logo.png";
+
 
 function Header() {
     return (
         <div className="bg-white shadow text-gray-800">
             <header className="container max-w-6xl px-10 py-3 mx-auto flex justify-between items-center">
                 <div className="flex items-center">
-                    <img className="mr-3" src={logo} alt="UCF School Logo"/>
-                    <h1 className="font-bold">Bootcamp Project</h1>
+                    <img className="mr-3"/>
+                    <h1 className="font-bold">Michigan State University Bootcamp Project</h1>
                 </div>
-                <a href="https://github.com/yarocruz/react-employee-directory" className="hover:text-yellow-500">Code</a>
+                <a className="hover:text-yellow-500"></a>
             </header>
         </div>
     )
